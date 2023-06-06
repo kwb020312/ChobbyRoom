ac0f93005d37823d16b33fafc95402b0b63dcecf
N S <kwb020312@naver.com>
2023-06-06 21:02:51 +0900

    Feat: assets.js 생성

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    name: "room",
+    type: "glbModel",
+    path: "/models/Finale.glb",
+  },
+  {
+    name: "screen",
+    type: "videoTexture",
+    path: "/textures/kda.mp4",
+  },
+];
