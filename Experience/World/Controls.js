ee2ca1161ca89f49cb40cd5aca0455a3aa1e7ad6
N S <kwb020312@naver.com>
2023-06-11 21:39:43 +0900

    Feat: 마우스 커서 위치에 따른 회전 기능

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -1,6 +1,5 @@
 import * as THREE from "three";
 import Experience from "../Experience";
-import GSAP from "gsap";
 
 export default class Controls {
   constructor() {
@@ -9,21 +8,9 @@ export default class Controls {
     this.resources = this.experience.resources;
     this.time = this.experience.time;
     this.camera = this.experience.camera;
-
-    this.lerp = {
-      current: 0,
-      target: 0,
-      ease: 0.1,
-    };
   }
 
   resize() {}
 
-  update() {
-    this.lerp.current = GSAP.utils.interpolate(
-      this.lerp.current,
-      this.lerp.target,
-      this.lerp.ease
-    );
-  }
+  update() {}
 }
--- Experience/World/Room.js
+++ Experience/World/Room.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import Experience from "../Experience";
+import GSAP from "gsap";
 
 export default class Room {
   constructor() {
@@ -10,8 +11,15 @@ export default class Room {
     this.room = this.resources.items.room;
     this.actualRoom = this.room.scene;
 
+    this.lerp = {
+      current: 0,
+      target: 0,
+      ease: 0.1,
+    };
+
     this.setModal();
     this.setAnimation();
+    this.onMouseMove();
   }
 
   setModal() {
@@ -53,9 +61,25 @@ export default class Room {
     this.swim.play();
   }
 
+  onMouseMove() {
+    window.addEventListener("mousemove", (e) => {
+      this.rotation =
+        ((e.clientX - window.innerWidth / 2) * 2) / window.innerWidth;
+      this.lerp.target = this.rotation;
+    });
+  }
+
   resize() {}
 
   update() {
+    this.lerp.current = GSAP.utils.interpolate(
+      this.lerp.current,
+      this.lerp.target,
+      this.lerp.ease
+    );
+
+    this.actualRoom.rotation.y = this.lerp.current;
+
     this.mixer.update(this.time.delta * 0.0009);
   }
 }
