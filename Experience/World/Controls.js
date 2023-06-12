8cb983e2f11075e583aa01eb282f0e4603dcb9fb
N S <kwb020312@naver.com>
2023-06-12 21:43:32 +0900

    Feat: 스크롤 및 3D 오브젝트 이동

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -1,13 +1,33 @@
 import * as THREE from "three";
 import Experience from "../Experience";
+import GSAP from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default class Controls {
   constructor() {
     this.experience = new Experience();
     this.scene = this.experience.scene;
+    this.sizes = this.experience.sizes;
     this.resources = this.experience.resources;
     this.time = this.experience.time;
     this.camera = this.experience.camera;
+    this.room = this.experience.world.room.actualRoom;
+    GSAP.registerPlugin(ScrollTrigger);
+    this.setPath();
+  }
+
+  setPath() {
+    this.timeline = new GSAP.timeline();
+    this.timeline.to(this.room.position, {
+      x: this.sizes.width * 0.00094,
+      scrollTrigger: {
+        trigger: ".first-move",
+        markers: true,
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 0.6,
+      },
+    });
   }
 
   resize() {}
