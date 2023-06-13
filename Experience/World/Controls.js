d8cfdd3821cd02718e75ee04c095f16bfec7a37c
N S <kwb020312@naver.com>
2023-06-13 23:10:05 +0900

    Feat: 스크롤에 따른 애니메이션 구현

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -12,11 +12,265 @@ export default class Controls {
     this.time = this.experience.time;
     this.camera = this.experience.camera;
     this.room = this.experience.world.room.actualRoom;
+    this.room.children.forEach((child) => {
+      if (child.type === "RectAreaLight") {
+        this.rectLight = child;
+      }
+    });
+
     GSAP.registerPlugin(ScrollTrigger);
     this.setScrollTrigger();
   }
 
-  setScrollTrigger() {}
+  setScrollTrigger() {
+    ScrollTrigger.matchMedia({
+      // 데스크톱
+      "(min-width: 969px)": () => {
+        this.room.scale.set(0.11, 0.11, 0.11);
+        this.rectLight.width = 0.5;
+        this.rectLight.height = 0.7;
+
+        // 첫번째 섹션
+        this.firstMoveTimeline = new GSAP.timeline({
+          scrollTrigger: {
+            trigger: ".first-move",
+            start: "top top",
+            end: "bottom bottom",
+            scrub: 0.6,
+            invalidateOnRefresh: true,
+          },
+        });
+        this.firstMoveTimeline.to(this.room.position, {
+          x: () => this.sizes.width * 0.0014,
+        });
+
+        // 두번째 섹션
+        this.secondMoveTimeline = new GSAP.timeline({
+          scrollTrigger: {
+            trigger: ".second-move",
+            start: "top top",
+            end: "bottom bottom",
+            scrub: 0.6,
+            invalidateOnRefresh: true,
+          },
+        })
+          .to(
+            this.room.position,
+            {
+              x: () => 1,
+              z: () => this.sizes.height * 0.0032,
+            },
+            "same"
+          )
+          .to(
+            this.room.scale,
+            {
+              x: 0.4,
+              y: 0.4,
+              z: 0.4,
+            },
+            "same"
+          )
+          .to(
+            this.rectLight,
+            {
+              width: 0.5 * 4,
+
+              height: 0.7 * 4,
+            },
+            "same"
+          );
+
+        // 세번째 섹션
+        this.thirdMoveTimeline = new GSAP.timeline({
+          scrollTrigger: {
+            trigger: ".third-move",
+            start: "top top",
+            end: "bottom bottom",
+            markers: true,
+            scrub: 0.6,
+            invalidateOnRefresh: true,
+          },
+        }).to(this.camera.orthographicCamera.position, {
+          y: -2,
+          x: -4.1,
+        });
+      },
+
+      // 모바일
+      "(max-width: 968px)": () => {
+        // 리셋
+        this.room.scale.set(0.07, 0.07, 0.07);
+        this.room.position.set(0, 0, 0);
+        this.rectLight.width = 0.3;
+        this.rectLight.height = 0.3;
+
+        // 첫번째 섹션
+        this.firstMoveTimeline = new GSAP.timeline({
+          scrollTrigger: {
+            trigger: ".first-move",
+            start: "top top",
+            end: "bottom bottom",
+            scrub: 0.6,
+            invalidateOnRefresh: true,
+          },
+        }).to(this.room.scale, {
+          x: 0.1,
+          y: 0.1,
+          z: 0.1,
+        });
+        // 두번째 섹션
+        this.secondMoveTimeline = new GSAP.timeline({
+          scrollTrigger: {
+            trigger: ".second-move",
+            start: "top top",
+            end: "bottom bottom",
+            scrub: 0.6,
+            invalidateOnRefresh: true,
+          },
+        })
+          .to(
+            this.room.scale,
+            {
+              x: 0.25,
+              y: 0.25,
+              z: 0.25,
+            },
+            "same"
+          )
+          .to(
+            this.rectLight,
+            {
+              width: 0.3 * 3.4,
+              height: 0.4 * 3.4,
+            },
+            "same"
+          )
+          .to(
+            this.room.position,
+            {
+              x: 1.5,
+            },
+            "same"
+          );
+        // 세번째 섹션
+        this.thirdMoveTimeline = new GSAP.timeline({
+          scrollTrigger: {
+            trigger: ".third-move",
+            start: "top top",
+            end: "bottom bottom",
+            markers: true,
+            scrub: 0.6,
+            invalidateOnRefresh: true,
+          },
+        }).to(this.room.position, {
+          z: -4.5,
+        });
+      },
+
+      // 모든 기기
+      all: () => {
+        // 나머지 플랫폼
+        this.secondPartTimeline = new GSAP.timeline({
+          scrollTrigger: {
+            trigger: ".third-move",
+            start: "center center",
+          },
+        });
+
+        this.room.children.forEach((child) => {
+          if (child.name === "Mini_Floor") {
+            this.first = GSAP.to(child.position, {
+              x: -5.44055,
+              z: 13.6135,
+              duration: 0.3,
+            });
+          }
+
+          if (child.name === "Mailbox") {
+            this.second = GSAP.to(child.scale, {
+              x: 1,
+              y: 1,
+              z: 1,
+              duration: 0.3,
+            });
+          }
+
+          if (child.name === "Lamp") {
+            this.third = GSAP.to(child.scale, {
+              x: 1,
+              y: 1,
+              z: 1,
+              duration: 0.3,
+            });
+          }
+
+          if (child.name === "FloorFirst") {
+            this.fourth = GSAP.to(child.scale, {
+              x: 1,
+              y: 1,
+              z: 1,
+              duration: 0.3,
+            });
+          }
+
+          if (child.name === "FloorSecond") {
+            this.fifth = GSAP.to(child.scale, {
+              x: 1,
+              y: 1,
+              z: 1,
+              duration: 0.3,
+            });
+          }
+
+          if (child.name === "FloorThird") {
+            this.sixth = GSAP.to(child.scale, {
+              x: 1,
+              y: 1,
+              z: 1,
+              duration: 0.3,
+            });
+          }
+
+          if (child.name === "Dirt") {
+            this.seventh = GSAP.to(child.scale, {
+              x: 1,
+              y: 1,
+              z: 1,
+              duration: 0.3,
+            });
+          }
+
+          if (child.name === "Flower1") {
+            this.eighth = GSAP.to(child.scale, {
+              x: 1,
+              y: 1,
+              z: 1,
+              duration: 0.3,
+            });
+          }
+
+          if (child.name === "Flower2") {
+            this.nineth = GSAP.to(child.scale, {
+              x: 1,
+              y: 1,
+              z: 1,
+              duration: 0.3,
+            });
+          }
+        });
+        this.secondPartTimeline.add(this.first);
+        this.secondPartTimeline.add(this.second);
+        this.secondPartTimeline.add(this.third);
+        this.secondPartTimeline.add(this.fourth, "-=0.2");
+        this.secondPartTimeline.add(this.fifth, "-=0.2");
+        this.secondPartTimeline.add(this.sixth, "-=0.2");
+        this.secondPartTimeline.add(this.seventh, "-=0.2");
+        this.secondPartTimeline.add(this.eighth);
+        this.secondPartTimeline.add(this.nineth, "-=0.1");
+      },
+    });
+  }
 
   resize() {}
 
--- Experience/World/Room.js
+++ Experience/World/Room.js
@@ -51,6 +51,24 @@ export default class Room {
           map: this.resources.items.screen,
         });
       }
+
+      if (child.name === "Mini_Floor") {
+        child.position.x = -0.289521;
+        child.position.z = 8.83572;
+      }
+
+      if (
+        child.name === "Mailbox" ||
+        child.name === "Lamp" ||
+        child.name === "FloorFirst" ||
+        child.name === "FloorSecond" ||
+        child.name === "FloorThird" ||
+        child.name === "Dirt" ||
+        child.name === "Flower1" ||
+        child.name === "Flower2"
+      ) {
+        child.scale.set(0, 0, 0);
+      }
     });
 
     const width = 0.5;
